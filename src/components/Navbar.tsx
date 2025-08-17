6de7de8c3cec31e1031df3f95461595327b027b0
amankumar033 <amankumar51462@gmail.com>
2025-08-17 16:24:37 +0530

    updated

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -280,7 +280,7 @@ const Navbar = () => {
               >
                 <div className="relative inline-block">
                   <img 
-                    src="/cart-2.png" 
+                    src="/shopping-cart.png" 
                     alt="Cart" 
                     className="w-8 h-8"
                   />
@@ -507,7 +507,7 @@ const Navbar = () => {
                   className="flex items-center justify-center bg-white/10 rounded-md py-3 text-sm"
                   showSpinner={false}
                 >
-                  <img src="/cart-2.png" alt="Cart" className="w-5 h-5 mr-2" />
+                  <img src="/shopping-cart.png" alt="Cart" className="w-5 h-5 mr-2" />
                   Cart ({cartItemCount})
                 </LoadingButton>
               </>
@@ -529,7 +529,7 @@ const Navbar = () => {
                     className="flex items-center justify-center bg-white/10 rounded-md py-3 text-sm w-full"
                     showSpinner={false}
                   >
-                    <img src="/cart-2.png" alt="Cart" className="w-5 h-5 mr-2" />
+                    <img src="/shopping-cart.png" alt="Cart" className="w-5 h-5 mr-2" />
                     Cart ({cartItemCount})
                   </LoadingButton>
                 </div>
